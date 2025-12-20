c715bebc341e495b11c05330696ef1349a83f74c
ivantgm <ivantgm@gmail.com>
2025-12-20 19:19:58 -0300

    consulta por codigo

--- consulta_api.py
+++ consulta_api.py
@@ -2,8 +2,10 @@ import json
 import urllib.request
 
 URL_POST = "https://miliogo.com/cupom/consulta.php"
+#URL_POST = "http://localhost/miliogo/cupom/consulta.php"
 
-data = {"nome": "ovos"}
+#data = {"nome": "ovos"}
+data = {"codigo": "7894474002638"}
 
 json_bytes = json.dumps(data, ensure_ascii=False).encode("utf-8")
 req = urllib.request.Request(
--- miliogo/cupom/consulta.php
+++ miliogo/cupom/consulta.php
@@ -17,8 +17,50 @@ if (!$data) {
 }
 
 $nome = $data["nome"] ?? "";
+$codigo = $data["codigo"] ?? "";
 
-if ($nome !== "") {
+if ($codigo !== "") {
+    $sql = "
+        SELECT 
+            i.codigo AS codigo,
+            i.descricao AS nome, 
+            i.un AS un,
+            round(i.valor_unit-(i.desconto/i.qtde), 2) AS valor, 
+            round(i.desconto/i.qtde, 2) AS desconto,
+            CONCAT(
+            SUBSTRING(c.data_hora_emissao, 7, 2), '/', 
+            SUBSTRING(c.data_hora_emissao, 5, 2), '/', 
+            SUBSTRING(c.data_hora_emissao, 1, 4)
+            ) AS data,
+            e.nome AS emitente
+        FROM cupom_item i
+        JOIN cupom c ON c.id = i.id_cupom
+        LEFT JOIN emitente e ON e.cnpj = c.cnpj_emitente
+        WHERE i.codigo = ?        
+        GROUP BY i.descricao, i.valor_unit, i.desconto, c.data_hora_emissao, e.nome
+        ORDER BY c.data_hora_emissao DESC
+    ";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $codigo);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $arr = [];
+    while ($row = $result->fetch_assoc()) {
+        $arr[] = [
+            "codigo" => $row["codigo"],
+            "nome" => $row["nome"],
+            "un" => $row["un"],
+            "valor" => $row["valor"],
+            "desconto" => $row["desconto"],
+            "data" => $row["data"],
+            "emitente" => $row["emitente"]
+        ];
+    }
+    echo json_encode($arr);
+    $stmt->close();
+    $conn->close();
+    exit;
+} else if ($nome !== "") {
     $sql = "
         SELECT 
             i.codigo AS codigo,
